eb6d173a1550d94192ea84029f90c4c3bb1d88b0
MOULIM994 <114001695+MOULIM994@users.noreply.github.com>
2023-05-18 22:20:01 +0530

    Create People XML file.m

---
+++ 1.2 How to sort xml code using XML dom and linq based on hierarchy/People XML file.m
@@ -0,0 +1,43 @@
+
+<people>
+ <person>
+  <name>John Smith</name>
+  <age>35</age>
+  <gender>Male</gender>
+ </person>
+ <person>
+  <name>Mary Johnson</name>
+  <age>28</age>
+  <gender>Female</gender>
+ </person>
+ <person>
+  <name>James Lee</name>
+  <age>42</age>
+  <gender>Male</gender>
+ </person>
+ <person>
+  <name>Sarah Kim</name>
+  <age>19</age>
+  <gender>Female</gender>
+ </person>
+ <person>
+  <name>Michael Chen</name>
+  <age>55</age>
+  <gender>Male</gender>
+ </person>
+ <person>
+  <name>Lisa Wong</name>
+  <age>31</age>
+  <gender>Female</gender>
+ </person>
+ <person>
+  <name>David Park</name>
+  <age>47</age>
+  <gender>Male</gender>
+ </person>
+ <person>
+  <name>Emily Kim</name>
+  <age>24</age>
+  <gender>Female</gender>
+ </person>
+</people>
