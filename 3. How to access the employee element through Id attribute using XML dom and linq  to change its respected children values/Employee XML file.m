f44a96c8dc301f19383a0eef22cf5a700db9a49d
MOULIM994 <114001695+MOULIM994@users.noreply.github.com>
2023-05-18 22:38:28 +0530

    Create Employee XML file.m

---
+++ 3. How to access the employee element through Id attribute using XML dom and linq  to change its respected children values/Employee XML file.m
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<employees>
+  <employee id="1">
+    <name>John Doe</name>
+    <age>30</age>
+    <department>Sales</department>
+    <salary>50000</salary>
+  </employee>
+  <employee id="2">
+    <name>Jane Doe</name>
+    <age>25</age>
+    <department>Marketing</department>
+    <salary>40000</salary>
+  </employee>
+</employees>
